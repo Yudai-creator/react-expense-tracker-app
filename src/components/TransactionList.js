d4c319801c7357e4981fc1933050a0674af8daab
Yudai-creator <perezheber73@gmail.com>
2022-06-01 23:14:21 -0400

    pulling out global context and rendering data out of it

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,11 +1,20 @@
+
+import { useContext } from 'react';
+import {GlobalContext} from '../context/GlobalState'
+
 const TransactionList = () => {
+
+    const {transactions} = useContext(GlobalContext)
+
     return ( 
         <>
             <h3>History</h3>
             <ul id="list" className="list">
-                <li className="minus">
-                Cash <span>-$400</span><button className="delete-btn">x</button>
-                </li> 
+                {transactions.map(transaction => (
+                    <li key={transaction.id} className="minus">
+                        {transaction.text} <span>{transaction.amount}</span><button className="delete-btn">x</button>
+                    </li> 
+                ))}
             </ul>
         </>
      );
