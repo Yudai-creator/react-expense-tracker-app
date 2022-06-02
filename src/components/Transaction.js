51cca0571b1595c7c483d38318bb7d62b392eb61
Yudai-creator <perezheber73@gmail.com>
2022-06-01 23:20:22 -0400

    transaction component handling global state

---
+++ src/components/Transaction.js
@@ -0,0 +1,14 @@
+
+
+const Transaction = ({transaction}) => {
+
+    
+
+    return ( 
+        <li className="minus">
+            {transaction.text} <span>{transaction.amount}</span><button className="delete-btn">x</button>
+        </li> 
+     );
+}
+ 
+export default Transaction;
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,4 +1,4 @@
-
+import Transaction from './Transaction';
 import { useContext } from 'react';
 import {GlobalContext} from '../context/GlobalState'
 
@@ -11,9 +11,7 @@ const TransactionList = () => {
             <h3>History</h3>
             <ul id="list" className="list">
                 {transactions.map(transaction => (
-                    <li key={transaction.id} className="minus">
-                        {transaction.text} <span>{transaction.amount}</span><button className="delete-btn">x</button>
-                    </li> 
+                    <Transaction key={transaction.id} transaction={transaction}/>
                 ))}
             </ul>
         </>
